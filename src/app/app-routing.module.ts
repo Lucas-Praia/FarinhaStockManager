0410ae5ac5eb01ab6951311ee2f3f888a5b11e16
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2024-09-16 16:37:33 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {DashboardComponent} from "./modules/dashboard/dashboard.component";
+import {ProdutosComponent} from "./modules/produtos/produtos.component";
+import {VendaComponent} from "./modules/venda/venda.component";
+import {HistoricoComponent} from "./modules/historico/historico.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'produtos', component: ProdutosComponent},
+  {path: 'venda', component: VendaComponent},
+  {path: 'historico', component: HistoricoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './modules/menubar/menubar.component';
+import {TabMenuModule} from "primeng/tabmenu";
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { ProdutosComponent } from './modules/produtos/produtos.component';
+import { VendaComponent } from './modules/venda/venda.component';
+import { HistoricoComponent } from './modules/historico/historico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent,
+    DashboardComponent,
+    ProdutosComponent,
+    VendaComponent,
+    HistoricoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
